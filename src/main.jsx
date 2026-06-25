7df8cf8f5fbe80cf3b13d18a671988b43cd0adfc
AlexDevLab-Hub <alexandre.massal@outlook.fr>
2026-06-25 08:33:38 +0000

    Fix GitHub Pages router basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename="/Surface-sp-cifique-pour-les-d-butants-">
+    <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
